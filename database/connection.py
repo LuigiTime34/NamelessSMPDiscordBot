fe811bfab73258f67106a07e0fff07bf20a24ae2
LuigiTime34 <lnorvell255@protonmail.com>
2025-04-29 12:49:42 -0400

    Packaged things :)

--- database/connection.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import os
-from const import DATABASE_PATH
-
-def get_connection():
-    """Create and return a database connection."""
-    conn = sqlite3.connect(DATABASE_PATH)
-    return conn
